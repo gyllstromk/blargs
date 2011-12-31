2286c9b793cb04e0b88b214962cf6ebd1aeb0b85
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2011-12-31 11:59:49 +0100

    added to setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='blargs',
       version='0.1',
       py_modules=['blargs', 'bconfig'],
       description='Blargs command line parser',
+      author='Karl Gyllstrom',
       author_email='karl.gyllstrom+blargs@gmail.com',
       url='https://bitbucket.org/gyllstromk/blargs',
      )
