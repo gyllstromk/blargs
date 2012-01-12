7cae46bab240527ff033c39f89ea120c4c70cf2e
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2012-01-12 14:21:04 +0100

    new version; notes

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
 
 setup(name='blargs',
-      version='0.2b',
+      version='0.2.1b',
       py_modules=['blargs'],
       description='Blargs command line parser',
       author='Karl Gyllstrom',
       author_email='karl.gyllstrom+blargs@gmail.com',
       url='https://bitbucket.org/gyllstromk/blargs',
      )
+
+# deploy
+# export LC_ALL='en.UTF-8'
+# python setup.py sdist upload
