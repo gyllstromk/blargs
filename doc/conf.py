e5e725dd12de043dc8e2334d7d3b992d6f974314
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2012-05-22 14:39:10 -0500

    fixed version

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'blargs'
-copyright = u'2011, Karl Gyllstrom'
+copyright = u'2012, Karl Gyllstrom'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
